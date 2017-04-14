037b0b2e83c90e1ca9fdd719b295f497e008efa5
Philippe Garneau <philippegarneau1@gmail.com>
2017-04-13 22:18:45 -0400

    Setup

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,35 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+# This script allows you to easily and quickly generate and deploy your website
+# using Hugo to your personal GitHub Pages repository. This script requires a
+# certain configuration, run the `setup.sh` script to configure this. 
 
-# Build the project.
-hugo
-# Go To Public folder
-cd public
-# Add changes to git.
-git add -A
+# Set the English locale for the `date` command.
+export LC_TIME=en_US.UTF-8
 
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
+# The commit message.
+MESSAGE="Site rebuild $(date)"
+
+msg() {
+    printf "\033[1;32m :: %s\n\033[0m" "$1"
+}
+
+if [[ $(git status -s) ]]; then
+    msg "The working directory is dirty, please commit or stash any pending changes"
+    exit 1;
 fi
-git commit -m "$msg"
 
-# Push source and build repos.
+msg "Removing the old website"
+pushd public
+git rm -rf *
+popd
+
+msg "Building the website"
+hugo
+
+msg "Pushing the updated \`public\` folder to the \`master\` branch"
+pushd public
+git add *
+git commit -m "$MESSAGE"
+popd
 git push origin master
-
-# Come Back
-cd ..
